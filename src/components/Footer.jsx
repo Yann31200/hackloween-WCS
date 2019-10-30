bb6e40e0519e3cc38985deaa1edd21b665560351
Yann Livingston <yann.livingston@gmail.com>
2019-10-30 16:24:05 +0100

    creation du footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Footer from './Footer.jsx';
+import './Footer.css';
+
+function () {
+    return (
+        <div className="footer-style">
+           <div className="footerp1">
+                <cite>Mention Illégales</cite>
+                <cite>A propos</cite>
+           </div>
+           <div className="footerp2">
+               <img src="logoFb"></img>
+               <img src="logoTwitter"></img>
+               <img src="logoInsta"></img>
+           </div>
+        </div>
+    )
+}
