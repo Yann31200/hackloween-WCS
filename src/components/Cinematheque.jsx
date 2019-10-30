545284a99d20010396fb389b0f31a8dc6320e71a
Damien Vayssiaire <damien.vayssiaire@gmail.com>
2019-10-30 18:20:54 +0100

    first header with routing

---
+++ src/components/Cinematheque.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import axios from 'axios';
+
+
+const Cinematheque = () => {
+    return (
+        <div>
+        <p>La vie c'est pas Arte, fuck, démoniaque comme Hortefeux </p>
+        </div>
+    );
+
+}
+
+export default Cinematheque;
