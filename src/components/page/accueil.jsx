fa5ec0dc2767953fd64782415be225d239c279b7
Yann Livingston <yann.livingston@gmail.com>
2019-11-03 19:55:58 +0100

    actualisation css

--- src/components/page/accueil.jsx
+++ src/components/page/accueil.jsx
@@ -5,8 +5,8 @@ function Accueil ( {movie}) {
         return (
             < div className="container" >
                 <div className="presentation">
-                    <h1 className="title">On the flux</h1>
-                    <h2 className="title">Ce site référencie tous les films d'horreur les plus flippants du game</h2>
+                    <h1 className="h1">On the flux</h1>
+                    <h2 className="h2">Ce site référencie tous les films d'horreur les plus flippants du game</h2>
                     <p className="text">Nous sommes une équipe de quatre dev qui te proposent de découvrir notre sélection raffinée. </p>
                     <h3 className="title">En voici une brève sélection ma p'tite flipette</h3>
                 </div>
@@ -16,7 +16,6 @@ function Accueil ( {movie}) {
                     <img className="images" src="https://zupimages.net/up/19/44/6ihf.jpg" alt=""/>
                     <img className="images" src="https://zupimages.net/up/19/44/aupc.jpg" alt=""/>
                 </div>
-
             </div >
         )
     };
